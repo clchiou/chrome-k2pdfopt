e3b25c12a8bf2dac3bdd0be3bcb90028fdbc47d5
Che-Liang Chiou <clchiou@gmail.com>
2013-08-19 19:32:51 -0700

    Fix missing directory when build from fresh tree

--- getvar.sh
+++ getvar.sh
@@ -11,9 +11,9 @@ if [ -z "${1:-}" ]; then
 fi
 
 if [ "${NACL_GLIBC:-}" = "1" ]; then
-  nacl_libc=glibc
+  NACL_LIBC=glibc
 else
-  nacl_libc=newlib
+  NACL_LIBC=newlib
 fi
 
 # Because ${NACLPORTS_ROOT}/src/build_tools/nacl_env.sh parses command-line
@@ -27,6 +27,47 @@ if [ -z "${PACKAGE_NAME:-}" ]; then
   export PACKAGE_NAME=dummy
 fi
 
+# XXX common.sh assumes ${NACLPORTS_INCLUDE} must exist, which is NOT true
+# for a fresh nacl_sdk tree...
+create_directory() {
+  local OS_NAME=$(uname -s)
+  if [ $OS_NAME = "Darwin" ]; then
+    local OS_SUBDIR="mac"
+  elif [ $OS_NAME = "Linux" ]; then
+    local OS_SUBDIR="linux"
+  else
+    local OS_SUBDIR="win"
+  fi
+
+  if [ $NACL_ARCH = "arm" ]; then
+    local TOOLCHAIN_ARCH="arm"
+  else
+    local TOOLCHAIN_ARCH="x86"
+  fi
+
+  local TOOLCHAIN_DIR=${OS_SUBDIR}_${TOOLCHAIN_ARCH}_${NACL_LIBC}
+  local NACL_TOOLCHAIN_ROOT=${NACL_SDK_ROOT}/toolchain/${TOOLCHAIN_DIR}
+
+  if [ ${NACL_ARCH} = "pnacl" ]; then
+    local NACL_CROSS_PREFIX=pnacl
+  else
+    local NACL_CROSS_PREFIX=${NACL_ARCH}-nacl
+  fi
+
+  if [ "${NACL_ARCH}" = "pnacl" ]; then
+    local NACLPORTS_PREFIX=${NACL_TOOLCHAIN_ROOT}/usr
+  else
+    local NACLPORTS_PREFIX=${NACL_TOOLCHAIN_ROOT}/${NACL_CROSS_PREFIX}/usr
+  fi
+
+  local NACLPORTS_INCLUDE=${NACLPORTS_PREFIX}/include
+
+  if [ ! -d "${NACLPORTS_INCLUDE}" ]; then
+    mkdir -p ${NACLPORTS_INCLUDE}
+  fi
+}
+create_directory
+
 # XXX ${NACLPORTS_ROOT}/src/build_tools/common.sh emits output...
 # Just shut it up!
 source common.sh > /dev/null
@@ -35,13 +76,13 @@ case "${command}" in
   SENTINELS)
     sentinels_dir=${NACLPORTS_ROOT}/src/out/sentinels
     if [ ${NACL_DEBUG} = "1" ]; then
-      echo ${sentinels_dir}/${NACL_ARCH}_${nacl_libc}_debug
+      echo ${sentinels_dir}/${NACL_ARCH}_${NACL_LIBC}_debug
     else
-      echo ${sentinels_dir}/${NACL_ARCH}_${nacl_libc}
+      echo ${sentinels_dir}/${NACL_ARCH}_${NACL_LIBC}
     fi
     ;;
   NACL_LIBC)
-    echo ${nacl_libc}
+    echo ${NACL_LIBC}
     ;;
   *)
     eval "echo \$${command}"
